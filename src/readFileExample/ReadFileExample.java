b6628dcb9dfd76c8bedc64e8c029e503d5fada6b
minhcong213274 <gathidungru@gmail.com>
2023-04-13 10:42:01 +0700

    ReadFileExample

---
+++ src/readFileExample/ReadFileExample.java
@@ -0,0 +1,32 @@
+package readFileExample;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class ReadFileExample {
+
+    public  void readFileTxt(String path){
+       try {
+           BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+           String line;
+           int sum =0;
+           while ((line = bufferedReader.readLine()) != null){
+               System.out.println(line);
+               sum += Integer.parseInt(line);
+           }
+           System.out.println("sum" + sum);
+       } catch (Exception e) {
+           System.err.println("file don't exists");
+       }
+
+   }
+    public static void main(String[] args) {
+       Scanner scanner =new Scanner(System.in);
+       String path = scanner.nextLine();
+       ReadFileExample readFileExample = new ReadFileExample();
+       readFileExample.readFileTxt(path);
+
+
+    }
+}
